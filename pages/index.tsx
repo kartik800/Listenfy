f46e702e435a8c2dcb1fbc623422adf5a9419bcd
Kartik <er.kartik18102016@gmail.com>
2023-03-10 20:37:25 +0530

    listenfy

---
+++ pages/index.tsx
@@ -0,0 +1,40 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
+import Center from '../components/Center'
+import Player from '../components/Player'
+import { getSession, GetSessionParams } from 'next-auth/react'
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+      <Head>
+        <title>Listenfy</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className='flex '>
+        <Sidebar/>
+        <Center/>
+      </main>
+
+      <div className='sticky bottom-0'>
+        <Player/>
+      </div>
+    </div>
+  )
+}
+
+export async function getServerSideProps(context: GetSessionParams){
+  const session = await getSession(context);
+
+  return {
+    props:{
+      session
+    }
+  }
+
+
+}
+
+export default Home
+
